cded9ee20d2b04397d45b87aeb9dfcc58c012450
Raissa Nunes <raissan084@gmail.com>
2025-04-28 22:58:23 -0300

    Atualizações

--- listas/lista01.hs
+++ listas/lista01.hs
@@ -44,8 +44,21 @@ fourPower :: Int -> Int
 fourPower y = square (square y)
 
 -- questão 6
-iesimoTermo :: Int -> Double
-iesimoTermo i = fromIntegral (i + 1) * sqrt 6
+i :: Int -> Float
+i 0 =sqrt(6)
+i n = sqrt((6+i(n-1)))
+-- sqrt calcula a raiz quadrada
+
+-- Função para calcular o fatorial de um número
+fatorial' :: Integer -> Integer
+fatorial' 0 = 1
+fatorial' n = n * fatorial (n - 1)
+
+-- Função para calcular o número de combinações
+combinacoes :: Integer -> Integer -> Integer
+combinacoes m n
+  | m >= n = fatorial' m `div` (fatorial' n * fatorial' (m - n))
+  | otherwise = error "m deve ser maior ou igual a n"
 
 -- questão 8
 mdc :: Int -> Int -> Int
@@ -58,6 +71,18 @@ mdc m n
 lastDigit :: Int - > Int
 lastDigit n = n `mod` 10
 
+-- questão 11
+anyDigitAux :: Int->[a]->[a]
+anyDigitAux 0 (x:_) = [x]
+anyDigitAux x (a:b) = anyDigitAux (x-1) b
+
+anyDigit :: Int->Int->Int
+anyDigit x y = read (anyDigitAux x (show y))
+
+-- questão 12
+allDifferent :: Int -> Int -> Int -> Bool
+allDifferent m n p = ( m/=n) && (n/=p) && (p/=m) 
+
 -- questão 13
 howManyEqual :: Int -> Int -> Int -> Int
 howManyEqual a b c 
@@ -65,13 +90,125 @@ howManyEqual a b c
     | ( a /= b ) && (b /= c ) && ( a /= c) = 0
     | otherwise = 2  
 
+-- questão 14
+periodo::Int
+periodo = 7
+
+sales :: Int->Int
+sales 1 = 41
+sales 2 = 72
+sales 3 = 48
+sales 4 = 2
+sales 5 = 91
+sales 6 = 55
+sales 7 = 30
+sales _ = -1
+
+-- a) howManyLess valor comeco fim retorno
+howManyLess :: Int->Int->Int->Int
+howManyLess x a b
+    | a > b || sales a == -1 = 0
+    | sales a < x = 1 + howManyLess x (a+1) b 
+    | otherwise = howManyLess x (a+1) b
+
+-- b) Se há zeros no periodo
+noZerosInPeriod :: Int->Bool
+noZerosInPeriod a 
+    | sales a == -1 = True
+    | sales a == 0 = False 
+    | otherwise = noZerosInPeriod (a+1)
+
+
+-- c) Quantidade de vendas iguais a zero
+zeroInPeriod :: [Int]
+zeroInPeriod = zerosPeriodToList 1
+
+zerosPeriodToList :: Int->[Int]
+zerosPeriodToList a
+    | sales a == -1 = []
+    | sales a == 0 = zerosPeriodToList (a+1) ++ [a]
+    | otherwise = zerosPeriodToList (a+1)
+
+-- d) Retorna lista de dias que a venda foi menor que um parametro
+daysLassThan :: Int->[Int]
+daysLassThan a = daysLassThanToList a 1
+
+daysLassThanToList :: Int->Int->[Int]
+daysLassThanToList x a 
+    | sales a == -1 = []
+    | sales a < x = daysLassThanToList x (a+1) ++ [a]
+    | otherwise = daysLassThanToList x (a+1)
+
+-- questão 15
+fib :: Int -> Int
+fib 0 = 0
+fib 1 = 1
+fib x = fib (x-1) + fib (x-2)
+
+antiFib :: Int -> Int
+antiFib x = antiFibAUX x 0
+
+antiFibAUX :: Int -> Int -> Int
+antiFibAUX x y
+    | x == fib y = y
+    | x > fib y = antiFibAUX x (y+1)
+    | x < fib y = -1
+
 -- questão 16
 funny :: Int -> Int -> Int -> Bool
 funny x y z = (x > z) && (y < x)
+
+-- questão 17 - letra minuscula para maiuscula
+paraMaiuscula :: Char -> Char
+paraMaiuscula x
+  | ord x >= ord 'a' && ord x <= ord 'z' = chr (ord x - (ord 'a' - ord 'A'))
+  | otherwise = x
+-- letra maiuscula para minuscula
+import Data.Char
+paraMinuscula :: Char -> Char
+paraMinuscula x
+  | ord x >= ord 'A' && ord x <= ord 'Z' = chr (ord x + (ord 'a' - ord 'A'))
+  | otherwise = x
+
+-- questão 18
+charToNum::Char->Int
+charToNum x 
+    | isDigit x = ord x - ord '0' -- se for digito
+    | otherwise = -1  
+
+-- questão 19
+duplicate :: String -> Int -> String
+duplicate s n 
+    | n == 0 = ['-1']
+    | n == 1 = s
+    | otherwise = s ++ duplicate s (n-1) 
+
+-- questão 20
+compS :: Int -> Int
+compS [] = 0
+compS (_:xs) = 1 + contaS xs  
+
+pushRight :: String -> Int -> String
+pushRight s n 
+    | n < contaS s = s
+    | otherwise = addG (n- compS s) ++ s -- n é o novo comprimento total!
+
+addG :: Int -> String
+addG 0 = ""
+addG k = '>' : addG (k-1)
+
+-- questão 21 ???
 
 -- questão 22 
 inverte :: [Int] -> [Int]
 inverte [] = []
 inverte (x:xs) = inverte xs ++ [x]
 
--- 
+-- questão 22
+separa :: [Int] -> ([Int], [Int])
+separa [] = ([], [])
+separa (x:xs)
+    | x `mod` 2 == 0 = (x : pares, impares)
+    | otherwise      = (pares, x : impares)
+  where
+    (pares, impares) = separa xs
