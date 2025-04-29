63b28d49aca488375c5bc68e3ee985488c5f4200
Raissa Nunes <raissan084@gmail.com>
2025-04-29 00:12:18 -0300

    Estudando

--- aulasSala/revisaoProva.hs
+++ aulasSala/revisaoProva.hs
@@ -46,8 +46,9 @@ invert (x:xs) = invert xs ++ [x]
 
 {- 07 inverte elementos das listas internas -}
 invert1 [[Int]] -> [[Int]]
-invert [] = []
-invert (x:xs) = 
+invert1 [] = []
+invert1 (x:xs) = invert x : invert1 xs
+
 {- 08 função que exclui a penúltima ocorrência de um número na lista-}
 -------------------------------------------------------------
 {- Exercícios
@@ -100,7 +101,52 @@ f _ = 0
    Exercício: implemente essa função com apenas dois parâmetros e 
    fazendo uso de maxi no código interno-}
 
-{- media vendas}
+{- media vendas-}
 
 -----------------------------------------------------------------------------------
 {-SCRIPT O3 -}
+{- Assunto: listas e tuplas -}
+periodo::Int
+periodo = 7
+
+-- implementar função maxi 
+
+-- tabela de vendas
+vendas :: Int -> Int
+vendas 0 = 0
+vendas 1 = 41
+vendas 2 = 72
+vendas 3 = 48
+vendas 4 = 0
+vendas 5 = 91
+vendas 6 = 55
+vendas 7 = 30
+
+{- 01 função que retorna uma lista de vendas -}
+
+
+{- 02 função que retorna [[Int]] com listas de dia e venda -}
+
+  
+----------------------------------------------------------
+{- 03 função que ordena uma lista de inteiros -}
+--ordenaLista::[Int]->[Int]
+
+-------------------------------------------------------------------------
+{- 04 função que ordena [[Int]] pelo primeiro Int de cada lista  -}
+--ordenaListaLista::[[Int]]->[[Int]]
+
+---------------------------------------------------------------------------
+{- 05 função que ordena as listas internas de [[Int]] e, em seguida, ordena a [[Int]] -}
+-----------  tuplas --------------------------------------------------------
+{- 06 função que gera uma lista de tuplas com dia e venda -}
+{- 07 função que gera o total de vendas-}
+--totalVendasT::[(Int, Int)] -> Int
+
+
+{- 08 função que retorna a maior venda -}
+  
+{- 08-b como implementar com apenas os parâmetros? 
+  maiorVendaT::[(Int, Int)] -> Int  -}  
+  
+{- 09 função que retorna os dias das maiores vendas -}
